20389a5a38543efc346131a3045d44f5efe14fca
Leonardo <leo_fla_bsb@hotmail.com>
2019-02-28 22:58:26 -0300

    Versão inicial com suporte ao arduino

--- PHP/autenticar.php
+++ PHP/autenticar.php
@@ -1,3 +1,5 @@
 <?php
-echo 'aa';
+        ini_set('max_execution_time', 3);
+        $output= shell_exec('PowerShell -ExecutionPolicy Bypass -Command "'. __DIR__ .'\lerRFID.ps1"');
+        echo $output;
 ?>
