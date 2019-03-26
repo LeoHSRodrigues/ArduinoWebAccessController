f91ad99345a9a8e35b5ca10879dd408b4a41a485
Leonardo Henrique <leo23fla@gmail.com>
2019-03-25 22:43:09 -0300

    Mais algumas mudanças 2

--- arduinowebaccesscontroller.sql
+++ arduinowebaccesscontroller.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Mar-2019 às 15:05
--- Versão do servidor: 10.1.37-MariaDB
--- versão do PHP: 7.1.26
+-- Generation Time: 26-Mar-2019 às 02:34
+-- Versão do servidor: 10.1.38-MariaDB
+-- versão do PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -44,8 +44,15 @@ CREATE TABLE `log` (
 CREATE TABLE `setor` (
   `id` int(11) NOT NULL,
   `idSetor` int(11) NOT NULL,
-  `Sigla` varchar(10) COLLATE utf8_bin NOT NULL
+  `Sigla` varchar(50) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `setor`
+--
+
+INSERT INTO `setor` (`id`, `idSetor`, `Sigla`) VALUES
+(1, 1012, 'NCF - Núcleo de Controle e Fiscalização');
 
 -- --------------------------------------------------------
 
@@ -55,9 +62,16 @@ CREATE TABLE `setor` (
 
 CREATE TABLE `setorusuario` (
   `id` int(11) NOT NULL,
-  `CPF` int(11) NOT NULL,
+  `CPF` varchar(11) COLLATE utf8_bin NOT NULL,
   `idSetor` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Extraindo dados da tabela `setorusuario`
+--
+
+INSERT INTO `setorusuario` (`id`, `CPF`, `idSetor`) VALUES
+(1, '11111111111', 1012);
 
 -- --------------------------------------------------------
 
@@ -75,16 +89,17 @@ CREATE TABLE `usuario` (
   `tagRFID` varchar(128) COLLATE utf8_bin NOT NULL,
   `biometria` blob NOT NULL,
   `senha` varchar(128) COLLATE utf8_bin NOT NULL,
-  `senha4` varchar(128) COLLATE utf8_bin NOT NULL
+  `senha4` varchar(128) COLLATE utf8_bin NOT NULL,
+  `cargo` text COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nome`, `CPF`, `dataDeNascimento`, `tipoConta`, `status`, `tagRFID`, `biometria`, `senha`, `senha4`) VALUES
-(1, 'Leonardo Henrique', '11111111111', '1993-09-14', 'adm', 'Ativo', '9c9231467b54d21d4114af238f663bd214745f52544c99dd3af4356ec20976032f0086948391937f141539bc2b75cb3d0cd3bdf2cbce28e64e149015b1dce86b', '', 'bf16fadfbdf1f8450d143857c08a8acb70033cc48d18b22037e18a8ca7081f636a0b805e5241e5e30959a21ee773c3293251a34923a8e77925dd140116f25293', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db'),
-(2, 'José Bonifácio', '22222222222', '1994-07-14', 'func', 'Ativo', '9c9231467b54d21d4114af238f663bd214745f52544c99dd3af4356ec20976032f0086948391937f141539bc2b75cb3d0cd3bdf2cbce28e64e149015b1dce86b', '', 'bf16fadfbdf1f8450d143857c08a8acb70033cc48d18b22037e18a8ca7081f636a0b805e5241e5e30959a21ee773c3293251a34923a8e77925dd140116f25293', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db');
+INSERT INTO `usuario` (`id`, `nome`, `CPF`, `dataDeNascimento`, `tipoConta`, `status`, `tagRFID`, `biometria`, `senha`, `senha4`, `cargo`) VALUES
+(1, 'Leonardo Henrique', '11111111111', '1993-09-14', 'adm', 'Ativo', '732e4861d97b4fcfeda4d7e12e6d4b8565ed78cbed1dc2d47aa4a326dff69c2eb66f40b6f874d757ba77f499883cb99dd36508c39e2a7f3f3dea333a4f2b7ac9', '', 'bf16fadfbdf1f8450d143857c08a8acb70033cc48d18b22037e18a8ca7081f636a0b805e5241e5e30959a21ee773c3293251a34923a8e77925dd140116f25293', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Gerente'),
+(2, 'José Bonifácio', '22222222222', '1994-07-14', 'func', 'Ativo', 'aaa', '', 'bf16fadfbdf1f8450d143857c08a8acb70033cc48d18b22037e18a8ca7081f636a0b805e5241e5e30959a21ee773c3293251a34923a8e77925dd140116f25293', 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', '');
 
 --
 -- Indexes for dumped tables
@@ -129,13 +144,13 @@ ALTER TABLE `log`
 -- AUTO_INCREMENT for table `setor`
 --
 ALTER TABLE `setor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `setorusuario`
 --
 ALTER TABLE `setorusuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `usuario`
